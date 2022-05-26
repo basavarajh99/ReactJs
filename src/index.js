47c7a0296d39ade2c1c6ced3f9fd0570cdf08afd
Basavaraj H <bsh91083645@gmail.com>
2022-05-26 01:29:48 -0700

    Configuring react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // to override changes that we make on index.css
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
