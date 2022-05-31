7a0c204942a23d8b835345bf991d71c4b5805ab9
Basavaraj H <bsh91083645@gmail.com>
2022-05-31 19:16:21 +0530

    React Redux Forms Persistance

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Row, Label, Col,} from 'reactstrap';
 import {Link} from 'react-router-dom';
-import {Control, LocalForm, Errors} from 'react-redux-form';
+import {Control, Form, actions, Errors} from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -20,6 +20,7 @@ class Contact extends Component {
 
     handleSubmit(values) {
         alert("Current State is:" +JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
     render() {
@@ -65,7 +66,7 @@ class Contact extends Component {
                     <div className='col-12'>
                         <h3>Send Us Your Feedback</h3>
                         <div className='col-12 col-md-9'>
-                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                                 <Row className="form-group">
                                     <Label htmlFor="firstname" md={2}>First Name</Label>
                                     <Col md={10}>
@@ -178,7 +179,7 @@ class Contact extends Component {
                                         <Button type="submit" color="primary">Send Feedback</Button>
                                     </Col>
                                 </Row>
-                            </LocalForm>
+                            </Form>
                         </div>
                     </div>
                 </div>
